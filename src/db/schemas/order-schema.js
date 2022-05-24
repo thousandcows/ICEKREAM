cbe65f2c0762f942bd295bc75ee5146b3f6ec0c6
shbaek1997 <shbaek1997@gmail.com>
2022-05-24 23:47:49 +0900

    Feat 추가 order-schema

---
+++ src/db/schemas/order-schema.js
@@ -0,0 +1,44 @@
+import { Schema } from 'mongoose';
+
+const OrderSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    deliveryAddress: {
+      type: new Schema(
+        {
+          postalCode: String,
+          address1: String,
+          address2: String,
+        },
+        {
+          _id: false,
+        }
+      ),
+      required: true,
+    },
+    billingMethod:{
+        type:String,
+        required: true,
+    },
+    paymentStatus:{
+        type:String,
+        required:true,
+    },
+    productList:[
+        {
+            id:String,
+            quantity:Number,
+        }
+    ],
+
+  },
+  {
+    collection: 'users',
+    timestamps: true,
+  }
+);
+
+export { OrderSchema };
