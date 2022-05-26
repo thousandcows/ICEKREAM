2e39e587d206e12be95c5e9db3fdd99ed0a8deb1
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-05-26 14:10:08 +0900

    DESIGN: 장바구니 페이지 1차 디자인

--- src/views/cart/product.js
+++ src/views/cart/product.js
@@ -1,51 +1,60 @@
 export default function Product(id, product) {
     this.id = id;
     this.product = product;
-
+  
     this.template = function () {
-        const div = document.createElement('div');
-        div.classList.add('product-card');
-        div.id = this.id;
-        div.innerHTML = `
-            <input type='checkbox' class='select-btn'>
-            <div>${this.product.name}</div>
-            <div>${this.product.price}</div>
-            <button class='delete-btn' >X</button>
-        `;
-        this.setEvent(div);
-        return div;
+      const div = document.createElement("li");
+      div.classList.add("items");
+      div.id = this.id;
+      div.innerHTML = `
+          <div class="infoWrap">
+              <input type="checkbox" class="select-btn"/>
+              <div class="cartSection">
+                  <img src="./product.png" alt="" class="itemImg" />
+                  <h3>${product.name}</h3>
+                  <p><input type="text" class="qty" placeholder="3" /> x ${product.price}</p>
+                  <p class="stockStatus">In Stock</p>
+              </div>
+              <div class="prodTotal cartSection">
+                  <p>${product.price}</p>
+              </div>
+              <div class="cartSection removeWrap">
+                  <a href="#" class="remove">x</a>
+              </div>
+          </div>
+      `;
+      this.setEvent(div);
+      return div;
     };
-
+  
     this.setEvent = function (elem) {
-        const deleteBtn = elem.querySelector('.delete-btn');
-        const selectInput = elem.querySelector('.select-btn');
-
-        deleteBtn.addEventListener('click', this.del.bind(this));
-        selectInput.addEventListener('change', this.select.bind(this));
+      const deleteBtn = elem.querySelector(".remove");
+      const selectInput = elem.querySelector(".select-btn");
+      deleteBtn.addEventListener("click", this.del.bind(this));
+      selectInput.addEventListener("change", this.select.bind(this));
     };
-
+  
     this.select = function () {
-        const total = document.getElementById('total');
-        const price = document.getElementById('price');
-        const prevtotal = Number(total.innerText);
-        const prevprice = Number(price.innerText);
-        const input = document
-            .getElementById(this.id)
-            .querySelector('.select-btn');
-        if (input.checked) {
-            total.innerText = prevtotal + 1;
-            price.innerText = prevprice + this.product.price;
-        } else {
-            total.innerText = prevtotal - 1;
-            price.innerText = prevprice - this.product.price;
-        }
+      const total = document.getElementById("total");
+      const price = document.getElementById("price");
+      const prevtotal = Number(total.innerText);
+      const prevprice = Number(price.innerText);
+      const input = document.getElementById(this.id).querySelector(".select-btn");
+      if (input.checked) {
+        total.innerText = prevtotal + 1;
+        price.innerText = prevprice + this.product.price;
+      } else {
+        total.innerText = prevtotal - 1;
+        price.innerText = prevprice - this.product.price;
+      }
     };
-
+  
     this.del = function () {
-        const productContainer = document.getElementById('product-container');
-        const cart = JSON.parse(localStorage.getItem('cart'));
-        delete cart[this.id];
-        localStorage.setItem('cart', JSON.stringify(cart));
-        productContainer.removeChild(document.getElementById(this.id));
+      const productContainer = document.querySelector(".cart");
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      delete cart[this.id];
+      localStorage.setItem("cart", JSON.stringify(cart));
+      productContainer.removeChild(document.getElementById(this.id));
     };
-}
+  }
+  
