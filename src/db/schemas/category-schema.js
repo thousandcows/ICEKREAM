2f5f44fc62b0f8b7c07e26b39e248aaec1175b7a
thousandcows <milcamboo@gmail.com>
2022-05-26 19:06:25 +0900

    📝Style: 코드 포맷 변경 - prettier, eslint를 적용해 코드 포캣을 변경하였습니다.

--- src/db/schemas/category-schema.js
+++ src/db/schemas/category-schema.js
@@ -5,14 +5,16 @@ const CategorySchema = new Schema({
         type: String,
         required: true,
     },
-    products: [{
-        type: Schema.Types.ObjectId,
-        ref: 'products',
-    }],
-    size:{
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'products',
+        },
+    ],
+    size: {
         type: [String],
         required: true,
-    }
+    },
 });
 
 export { CategorySchema };
