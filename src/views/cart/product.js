76376696dc4b911bbd54a44dfb52f00e6bd57d9e
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-05-26 18:26:21 +0900

    FIX: 삭제 시 총 결제금액에서 제외되지 않는 버그 수정

--- src/views/cart/product.js
+++ src/views/cart/product.js
@@ -1,60 +1,90 @@
-export default function Product(id, product) {
+export default function Product(target, id, product) {
+    this.target = target;
     this.id = id;
     this.product = product;
-  
-    this.template = function () {
-      const div = document.createElement("li");
-      div.classList.add("items");
-      div.id = this.id;
-      div.innerHTML = `
+
+    this.template = function (idx) {
+        const li = document.createElement('li');
+        idx % 2 ? li.classList.add('items', 'even') : li.classList.add('items');
+        li.id = this.id;
+        li.innerHTML = `
           <div class="infoWrap">
               <input type="checkbox" class="select-btn"/>
               <div class="cartSection">
                   <img src="./product.png" alt="" class="itemImg" />
                   <h3>${product.name}</h3>
-                  <p><input type="text" class="qty" placeholder="3" /> x ${product.price}</p>
+                  <p><input type="text" class="qty" placeholder=${
+                      product.quantity
+                  } />x ${product.price}</p>
                   <p class="stockStatus">In Stock</p>
               </div>
               <div class="prodTotal cartSection">
-                  <p>${product.price}</p>
+                  <p>${product.price * product.quantity}</p>
               </div>
-              <div class="cartSection removeWrap">
+              <div class="cartSection remove-btn">
                   <a href="#" class="remove">x</a>
               </div>
           </div>
       `;
-      this.setEvent(div);
-      return div;
+        this.setEvent(li);
+        return li;
     };
-  
+
     this.setEvent = function (elem) {
-      const deleteBtn = elem.querySelector(".remove");
-      const selectInput = elem.querySelector(".select-btn");
-      deleteBtn.addEventListener("click", this.del.bind(this));
-      selectInput.addEventListener("change", this.select.bind(this));
+        const deleteBtn = elem.querySelector('.remove-btn');
+        const selectInput = elem.querySelector('.select-btn');
+        selectInput.addEventListener('change', this.select.bind(this));
+        deleteBtn.addEventListener('click', this.del.bind(this));
     };
-  
+
     this.select = function () {
-      const total = document.getElementById("total");
-      const price = document.getElementById("price");
-      const prevtotal = Number(total.innerText);
-      const prevprice = Number(price.innerText);
-      const input = document.getElementById(this.id).querySelector(".select-btn");
-      if (input.checked) {
-        total.innerText = prevtotal + 1;
-        price.innerText = prevprice + this.product.price;
-      } else {
-        total.innerText = prevtotal - 1;
-        price.innerText = prevprice - this.product.price;
-      }
+        const quantity = document.getElementById('quantity');
+        const subTotal = document.getElementById('subtotal');
+        const total = document.getElementById('total');
+        const input = document
+            .getElementById(this.id)
+            .querySelector('.select-btn');
+        if (input.checked) {
+            quantity.innerText =
+                Number(quantity.innerText) + this.product.quantity;
+            subTotal.innerText =
+                Number(subTotal.innerText) +
+                this.product.quantity * this.product.price;
+            total.innerText =
+                Number(total.innerText) +
+                this.product.quantity * this.product.price;
+        } else {
+            quantity.innerText =
+                Number(quantity.innerText) - this.product.quantity;
+            subTotal.innerText =
+                Number(subTotal.innerText) -
+                this.product.quantity * this.product.price;
+            total.innerText =
+                Number(total.innerText) -
+                this.product.quantity * this.product.price;
+        }
     };
-  
+
     this.del = function () {
-      const productContainer = document.querySelector(".cart");
-      const cart = JSON.parse(localStorage.getItem("cart"));
-      delete cart[this.id];
-      localStorage.setItem("cart", JSON.stringify(cart));
-      productContainer.removeChild(document.getElementById(this.id));
+        const quantity = document.getElementById('quantity');
+        const subTotal = document.getElementById('subtotal');
+        const total = document.getElementById('total');
+        const input = document
+            .getElementById(this.id)
+            .querySelector('.select-btn');
+        if (input.checked) {
+            quantity.innerText =
+                Number(quantity.innerText) - this.product.quantity;
+            subTotal.innerText =
+                Number(subTotal.innerText) -
+                this.product.quantity * this.product.price;
+            total.innerText =
+                Number(total.innerText) -
+                this.product.quantity * this.product.price;
+        }
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        delete cart[this.id];
+        localStorage.setItem('cart', JSON.stringify(cart));
+        this.target.removeChild(document.getElementById(this.id));
     };
-  }
-  
+}
--- src/views/register/register.js
+++ src/views/register/register.js
@@ -16,48 +16,50 @@ async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  submitButton.addEventListener('click', handleSubmit);
+    submitButton.addEventListener('click', handleSubmit);
 }
 
 // 회원가입 진행
 async function handleSubmit(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  const fullName = fullNameInput.value;
-  const email = emailInput.value;
-  const password = passwordInput.value;
-  const passwordConfirm = passwordConfirmInput.value;
+    const fullName = fullNameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const passwordConfirm = passwordConfirmInput.value;
 
-  // 잘 입력했는지 확인
-  const isFullNameValid = fullName.length >= 2;
-  const isEmailValid = validateEmail(email);
-  const isPasswordValid = password.length >= 4;
-  const isPasswordSame = password === passwordConfirm;
+    // 잘 입력했는지 확인
+    const isFullNameValid = fullName.length >= 2;
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid = password.length >= 4;
+    const isPasswordSame = password === passwordConfirm;
 
-  if (!isFullNameValid || !isPasswordValid) {
-    return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
-  }
+    if (!isFullNameValid || !isPasswordValid) {
+        return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
+    }
 
-  if (!isEmailValid) {
-    return alert('이메일 형식이 맞지 않습니다.');
-  }
+    if (!isEmailValid) {
+        return alert('이메일 형식이 맞지 않습니다.');
+    }
 
-  if (!isPasswordSame) {
-    return alert('비밀번호가 일치하지 않습니다.');
-  }
+    if (!isPasswordSame) {
+        return alert('비밀번호가 일치하지 않습니다.');
+    }
 
-  // 회원가입 api 요청
-  try {
-    const data = { fullName, email, password };
+    // 회원가입 api 요청
+    try {
+        const data = { fullName, email, password };
 
-    await Api.post('/api/register', data);
+        await Api.post('/api/register', data);
 
-    alert(`정상적으로 회원가입되었습니다.`);
+        alert(`정상적으로 회원가입되었습니다.`);
 
-    // 로그인 페이지 이동
-    window.location.href = '/login';
-  } catch (err) {
-    console.error(err.stack);
-    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-  }
+        // 로그인 페이지 이동
+        window.location.href = '/login';
+    } catch (err) {
+        console.error(err.stack);
+        alert(
+            `문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`,
+        );
+    }
 }
