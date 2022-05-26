7ef2aa62ff93d8d4fdabfa885a4b1570bc55ac7b
thousandcows <milcamboo@gmail.com>
2022-05-26 19:17:31 +0900

    FEAT📰: 추가 product-router 1. product 관련 로직을 처리하는 product-router를 구현하였습니다. 2. 현재 사용할 수 있는 기능은 다음과 같습니다. - 전체 상품 조회 - 카테고리별 상품 조회 - 상품 추가 3. 앞으로 추가할 기능 - 상품 상세 정보 조회 - 상품 내용 수정 - 상품 삭제 - 조회 기능에 조회수 필터링 적용

---
+++ src/routers/product-router.js
@@ -0,0 +1,104 @@
+import { Router } from 'express';
+import is from '@sindresorhus/is';
+import { productService } from '../services/product-service';
+import { CategorySchema } from '../db/schemas/category-schema';
+import { model } from 'mongoose';
+import { ProductSchema } from '../db/schemas/product-schema';
+
+const Category = model('categories', CategorySchema);
+
+const productRouter = Router();
+
+productRouter.get('/', async (req, res, next) => {
+    try {
+
+        const productList = await productService.findAllProducts();
+        
+        console.log(productList);
+
+        if (!productList){
+            res.status(200).json({"result": "no results"})
+        }
+        res.status(200).json(productList);
+    } catch (error) {
+        next(error);
+    }
+});
+
+productRouter.get('/:category', async (req, res, next) => {
+    try {
+        const category = req.params.category;
+        const productList = await productService.findByCategory(category);
+        
+        console.log(productList);
+
+        if (!productList){
+            res.status(200).json({"result": "no results"})
+        }
+
+        res.status(200).json(productList);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+productRouter.post('/add', async (req, res, next) => {
+    try {
+        if (is.emptyObject(req.body)) {
+            throw new Error(
+                'headers의 Content-Type을 application/json으로 설정해주세요',
+            );
+        }
+        const { category } = req.body;
+        const { brand } = req.body;
+        const { productName } = req.body;
+        const { price } = req.body;
+        const { launchDate } = req.body;
+        const { img } = req.body;
+        const { views } = req.body;
+        const { quantity } = req.body;
+        const { purchaseCount } = req.body;
+        
+        // Product atomicity check
+        const isProductExist = await productService.findOne({productName: productName});
+
+        if (isProductExist){
+            throw new Error('해당 상품은 이미 존재합니다. 다시 한 번 확인해주세요.');
+        }
+
+        const newProduct = await productService.addProduct({
+            brand : brand,
+            productName: productName,
+            price: price,
+            launchDate: launchDate,
+            img: img,
+            views: views,
+            quantity: quantity,
+            purchaseCount: purchaseCount,
+        });
+        console.log(newProduct);
+
+        // Category atomicity check
+        const isCategoryExist = await Category.findOne({name: category});
+
+        if (isCategoryExist){
+            await Category.updateOne(
+                { name: category },
+                { $push: { products: newProduct._id } },
+            );
+        } else{
+            await Category.create({
+                name: category,
+                products: [],
+                size: []
+            })
+        }
+
+        res.status(200).json(newProduct);
+    } catch (error) {
+        next(error);
+    }
+});
+
+export { productRouter };
