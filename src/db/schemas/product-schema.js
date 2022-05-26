52ae0f96ca6657761395cbbd47bb85a1198746c7
thousandcows <milcamboo@gmail.com>
2022-05-26 19:09:45 +0900

    Fix 📝: 수정 product-schema - product-schema의 내용을 수정하였습니다. - category 필드는 objectId를 활용해 category 다큐먼트를 참고합니다.

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -1,58 +1,56 @@
 import { Schema } from 'mongoose';
-const shortId = require('./types/short-id');
-
-// required: shortId, category, brand, nameEnglish, price, firstDateAvailable, img, views, quantity
-// optional: size
-// in consideration: purchaseCount, purchasedUser
 const ProductSchema = new Schema(
-  {
-    shortId,
-    category: {
-        type: String, // categories = SHOES, CLOTHES, ETC
-        required: true,
+    {
+        category: {
+            type: Schema.Types.ObjectId, // categories = SHOES, CLOTHES, ETC
+            ref: 'category',
+        },
+        brand: {
+            type: String,
+            required: true,
+        },
+        productName: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        launchDate: {
+            type: String,
+            required: true,
+            default: '2022-05-25',
+        },
+        img: {
+            type: String,
+            required: true,
+        },
+        views: {
+            type: Number,
+            rerquired: true,
+            default: 0,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 100,
+        },
+        purchaseCount: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        purchasedUsers: [String],
+        size:{
+            type: [String],
+        }
     },
-    brand: {
-        type: String,
-        required: true,
+    {
+        collection: 'products',
+        timestamps: true,
     },
-    productName: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    firstDateAvailable: {
-        type: String,
-        required: true,
-        default: "2022-05-25",
-    },
-    img: {
-        type: String,
-        required: true,
-    },
-    views: {
-        type: Number,
-        rerquired: true,
-        default: 0,
-    },
-    quantity: {
-        type: Number,
-        required: true,
-        default: 100,
-    },
-    purchaseCount: {
-        type: Number,
-        required: false,
-        default: 0
-    },
-    purchasedUsers: [String]
-  },
-  {
-    collection: 'products',
-    timestamps: true,
-  }
 );
 
 export { ProductSchema };
