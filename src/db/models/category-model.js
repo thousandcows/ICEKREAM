da91737994aeac4043fbac4f19738d39cd8cb3b9
thousandcows <milcamboo@gmail.com>
2022-05-29 10:40:04 +0900

    FIX🔨: 카테고리 모델 주석 수정
    1. 주석 순서와 내용을 수정했습니다.
    - 카테고리 CRUD 기능이 1 ~ 5번에 위치합니다.
    - 카테고리의 products 필드에 상품을 추가, 삭제하는 기능을 그 뒤에 구현합니다.
    
    Issue: #61

--- src/db/models/category-model.js
+++ src/db/models/category-model.js
@@ -4,40 +4,29 @@ import { CategorySchema } from '../schemas/category-schema';
 const Category = model('catagory', CategorySchema);
 
 export class CategoryModel {
-    
-    // 1. 상품 추가 시 카테고리에 반영
-    async addProductToCategory(categoryName, productId){
-        console.log(categoryName);
-        const filter = { name: categoryName};
-        const update = { $push : { products : productId}};
-        const option = { returnOriginal : false };
 
-        const updatedCategory = await Category.updateOne(filter, update, option);
-        return updatedCategory;
-    }
-
-    // 2. 새 카테고리 추가
+    // 1. 새 카테고리 추가
     async addCategory(categoryInfo){
         console.log("info: " + categoryInfo);
         const newCategory = await Category.create(categoryInfo);
         return newCategory;
     }
-    // 3. 카테고리 삭제
+    // 2. 카테고리 삭제
     async deleteCategory(categoryId){
         const result = await Category.deleteOne({_id: categoryId});
         return result;
     }
-    // 4. 카테고리 목록 조회
+    // 3. 전체 카테고리 조회
     async findAllCategories(){
         const categoyList = await Category.find({})
         return categoyList;
     }
-    // 5. 카테고리 검색
+    // 4. 카테고리 검색
     async findOne(categoryName){
         const catagory = await Category.findOne({name: categoryName});
         return catagory;
     }
-    // 6. 카테고리 수정
+    // 5. 카테고리 수정
     async updateCategory(categoryId, name, size){
         
         const filter = {_id: categoryId};
@@ -47,6 +36,17 @@ export class CategoryModel {
         const updateCategory = await Category.updateOne(filter, update, option);
         return updateCategory;
 
+    }
+
+    // 6. 상품 추가 시 카테고리에 반영
+    async addProductToCategory(categoryName, productId){
+        console.log(categoryName);
+        const filter = { name: categoryName};
+        const update = { $push : { products : productId}};
+        const option = { returnOriginal : false };
+
+        const updatedCategory = await Category.updateOne(filter, update, option);
+        return updatedCategory;
     }
 
 }
