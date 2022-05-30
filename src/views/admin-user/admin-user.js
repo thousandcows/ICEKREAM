5bdb91b2482011b516505dda21b725fd422119a5
shbaek1997 <shbaek1997@gmail.com>
2022-05-30 18:06:44 +0900

    Feat: 관리자 전용 유저 확인 페이지 기본 기능 구현 완료

--- src/views/admin-user/admin-user.js
+++ src/views/admin-user/admin-user.js
@@ -1,4 +1,16 @@
 import * as Api from '/api.js';
-console.log('hello');
-const users = await Api.get('api/admin/users');
-console.log(users);
+// 어떻게 해야지 user 가 접근했을 때 권한이 없으면 바로 redirect 할까...
+
+const users = await Api.get('', 'api/admin/users');
+const sectionContainer = document.querySelector('#section-container');
+users.forEach((user) => {
+    let userAddress = user.address
+        ? user.address.postalCode + user.address.address1
+        : 'not available';
+    let userContent = `<div class="table-column">${user.email}</div>
+    <div class="table-column">${user.fullName}</div>
+    <div class="table-column">${user.phoneNumber || 'not available'}</div>
+    <div class="table-column">${user.role || 'not available'}</div>
+    <div class="table-column">${userAddress}</div>`;
+    sectionContainer.innerHTML += userContent;
+});
