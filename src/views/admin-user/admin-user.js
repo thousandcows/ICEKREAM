0769bda3d926dcac657216a1cb1176678c037585
shbaek1997 <shbaek1997@gmail.com>
2022-05-30 22:21:10 +0900

    Feat: admin 유저관리 페이지에 유저 권한 수정 버튼 구현 완료

--- src/views/admin-user/admin-user.js
+++ src/views/admin-user/admin-user.js
@@ -1,16 +1,36 @@
 import * as Api from '/api.js';
 // 어떻게 해야지 user 가 접근했을 때 권한이 없으면 바로 redirect 할까...
 
+async function handleSwitchRole() {
+    const userId = this.id.slice(4);
+    if (this.innerHTML === 'basic-user') {
+        this.innerHTML = 'admin';
+    } else {
+        this.innerHTML = 'basic-user';
+    }
+    const updatedUser = await Api.patch('', `api/admin/${userId}`, {});
+    console.log(updatedUser);
+}
+
 const users = await Api.get('', 'api/admin/users');
 const sectionContainer = document.querySelector('#section-container');
 users.forEach((user) => {
     let userAddress = user.address
         ? user.address.postalCode + user.address.address1
         : 'not available';
-    let userContent = `<div class="table-column">${user.email}</div>
-    <div class="table-column">${user.fullName}</div>
+    let userEmail = user.email.slice(0, -9) + '*********';
+    let fullname = '*' + user.fullName.slice(1);
+    let userContent = `<div class="table-column">${userEmail}</div>
+    <div class="table-column">${fullname}</div>
     <div class="table-column">${user.phoneNumber || 'not available'}</div>
-    <div class="table-column">${user.role || 'not available'}</div>
+    <div class="table-column"><button id=user${user._id}>${
+        user.role
+    }</button></div>
     <div class="table-column">${userAddress}</div>`;
     sectionContainer.innerHTML += userContent;
+});
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', handleSwitchRole);
 });
