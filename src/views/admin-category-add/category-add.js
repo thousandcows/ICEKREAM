168ffcb0da041b4ccfa9c283da42103cfb6f7eed
thousandcows <milcamboo@gmail.com>
2022-06-01 01:34:01 +0900

    FEAT 📝: - 추가 alert message 및 redirection
    - 카테고리를 성공적으로 등록하면 알람 메시지가 뜨고, 관리자 페이지 메인으로 이동합니다.
    
    Issue: #78

--- src/views/admin-category-add/category-add.js
+++ src/views/admin-category-add/category-add.js
@@ -43,8 +43,10 @@ async function handleSubmit(e) {
 
             const result = await Api.post('/api/admin/product/category', data);
 
-            console.log(result);
-
+            if (result) {
+                alert(`${result.categoryName} 카테고리가 성공적으로 등록되었습니다!`);
+                window.location.href = '/admin';
+            }
         }catch(err){
             console.error(err.stack);
             alert(`${err.message}`);
