5f267a5d5878f6c245565d92ed4404cbae7c19bb
Kanghunk <ltbehappy16@gmail.com>
2022-06-03 12:53:49 +0900

    FEAT: NAV 수정중

--- src/views/admin/admin.js
+++
@@ -1,9 +0,0 @@
-import { navTransition } from '../nav-transition/nav-transition.js';
-
-
-navTransition('adminPage').then( checkData => {
-    if (!checkData.isLogined || !checkData.isAdmin) {
-        alert('관리자 로그인이 필요한 페이지 입니다...');
-        window.location.href = '/';
-    }
-});
